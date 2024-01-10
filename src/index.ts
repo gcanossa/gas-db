bd47c7dddd63904a0d5736c1298a18aa47f5bada
Gianmaria Canossa <gianmaria.canossa@gmail.com>
2024-01-10 12:20:18 +0100

    fix: sheet-orm exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-export {
-  createTableContext, 
-  createTableReadContext, 
-  type DataRangeDescriptor, 
-  type ColumnsMapping,
-  type EntityPropertyMapping,
-  type TableReadContext,
-  type TableWriteContext,
-  type TableContext } from './sheets-orm';
+export * from "./sheets-orm";
 
-export * from './drive-fs-util';
-export * from './key-value-store';
+export * from "./drive-fs-util";
+export * from "./key-value-store";
